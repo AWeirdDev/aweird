90c20519ebc934a62a0fd1864f1671d45ef60299
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-05-25 19:26:31 +0800

    :bar_chart: Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='aweird',
+    version='1.0',
+    packages=['aweird'],
+    license='MIT license',
+    long_description="Python text-detecting, routine functions all-in-one.",
+    author='AWeirdScratcher',
+    author_email = "aweirdscrather@gmail.com",
+    install_requires=[
+    'requests'
+    ]
+)
