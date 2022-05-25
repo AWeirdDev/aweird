13efb1656b58926f40caf45de1bfca02e511d225
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-05-25 18:40:02 +0800

    colors

---
+++ aweird/kit/colors.py
@@ -0,0 +1,19 @@
+
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[31m'
+    end = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+    """
+    Colors for terminal.
+    Example Usage:
+    ```py
+    from aweird.kit import Colors
+    print(Colors.okblue + "this is blue" + Colors.end)
+    ```
+    """
