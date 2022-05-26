d6bd53b1d8841e822309a902f55dbf66b412cbf9
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-05-26 12:10:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aweird',
-    version='1.0',
+    version='1.0a',
     packages=['aweird.kit'],
     license='MIT license',
     long_description="Python text-detecting, routine functions all-in-one.",
